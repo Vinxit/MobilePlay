a20025ed234c911b73d86246038541d0a5e0cb73
Vinxit <Vinxit_HK@163.com>
2017-10-09 18:10:38 +0800

    drawerlayout

--- app/src/main/java/com/vinxit/mobileplay/MainActivity.java
+++ app/src/main/java/com/vinxit/mobileplay/MainActivity.java
@@ -1,22 +1,36 @@
 package com.vinxit.mobileplay;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @BindView(R.id.drawelayout)
     DrawerLayout drawelayout;
-
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawelayout,toolbar,R.string.navigation1,R.string.navigation2);
+        drawelayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
